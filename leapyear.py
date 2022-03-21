277fd4644568c6b0693b36e8e5e4841b5ce8d930
m00tiny <42702437+m00tiny@users.noreply.github.com>
2022-03-21 01:36:46 -0400

    I made that WAAAY harder than it is

--- leapyear.py
+++ leapyear.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python3
-
 def is_leap(year):
     leap = False
-    check_leap = ((year // 400) % 2)
-    if check_leap == 0:
-        check_leap = ((year // 100) % 2)
-        if check_leap == 0:
-            return leap
-        else:
-            check_leap = ((year // 4) % 2)
-            if check_leap == 0:
+    if (year % 4) == 0:
+        leap = True
+        if (year % 100) == 0:
+            leap = False
+            if (year % 400) == 0:
                 leap = True
     return leap
 
-year = int(raw_input())
+year = int(input())
 print is_leap(year)
