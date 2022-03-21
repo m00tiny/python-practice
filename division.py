396e8c7f1764526c6f3a6964858684bf572c1425
m00tiny <42702437+m00tiny@users.noreply.github.com>
2022-03-20 23:30:01 -0400

    added division.py and shebang for basic-arithmetic.py

--- division.py
+++ division.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
+from __future__ import division
+
 if __name__ == '__main__':
     a = int(raw_input())
     b = int(raw_input())
-    print (a + b);
-    print (a - b);
-    print (a * b);
+    print (a // b);
+    print (a / b);
