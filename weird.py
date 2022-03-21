45d2e68f3d79fd0ab534ccc53791d904a8404a6e
m00tiny <42702437+m00tiny@users.noreply.github.com>
2022-03-20 23:24:42 -0400

    hackerrank weird.py

---
+++ weird.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(raw_input().strip())
+    weird = [6,8,10,12,14,16,18,20]
+    if n % 2 != 0:
+        print ("Weird");
+    elif n in weird:
+        print ("Weird");
+    else:
