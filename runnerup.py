1709f100f1ffa25dc1ab34ad34890e7e0e481138
m00tiny <m00tiny@users.noreply.github.com>
2022-03-23 09:59:16 -0400

    runner up

--- runnerup.py
+++ runnerup.py
@@ -2,7 +2,11 @@
 n = int(5)
 arr = [2, 3, 6, 6, 5]
 winner = max(arr)
-runnerup = winner - 1
-if runnerup not in arr:
-    runnerup = runnerup -1
-print(runnerup)
+runnerup = winner
+runnerup = runnerup - 1
+if runnerup in arr:
+    print (runnerup)
+else:
+    while runnerup in arr == False:
+        runnerup = runnerup -1
+    print(runnerup)
