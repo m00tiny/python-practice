305a221516066f3309470aeb982ed52cf2fc28a3
Stuart Gray <m00tiny@grayhatfreelancing.com>
2022-03-27 00:35:00 -0400

    greatest common denominator

---
+++ gcd.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import math
+
+print("Give me two numbers and I will return their greatest common denominator.")
+var1=int(input())
+var2=int(input())
+
+ans = var1 / var2
+#if ans % 2 == 0:
+print (ans)
+
